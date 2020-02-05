5b0f48bfd6860d36cc6c5c124795b47e54610946
1984aomame <kafka.on.the.shoree@gmail.com>
2020-02-05 10:48:19 +0900

    ファイル作成

---
+++ ContactForm/confirm.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>プログラミング講座申込み確認</title>
+</head>
+<body>
+<h2>プログラム体験申込確認</h2>
+<form id="confirmForm" method="POST" action ="http://localhost/ContactForm/register.php">
+    <!-- <input type="hidden" name="_token" value=""> -->
+    <form action="entry.php" method ="post">
+    <table>
+    <tr>
+     <td>氏名</td> 
+     <td><?php echo $_POST["name"]; ?></td>  
+    </tr>
+    <tr>
+     <td>電話番号</td> 
+     <td><?php echo $_POST["tel"]; ?></td>  
+    </tr>
+    <tr>
+     <td>メールアドレス</td>
+     <td><?php echo $_POST["mail"]; ?></td>   
+    </tr>
+    <tr>
+     <td>都道府県</td>
+     <td><?php echo $_POST["pref"]; ?></td>   
+    </tr>
+    <tr>
+     <td>年齢</td>
+     <td><?php echo $_POST["age"]; ?></td>   
+    </tr>
+    <tr>
+     <td>備考欄</td> 
+     <td><?php echo $_POST["message"]; ?></td>  
+    </tr>
+    </table>
+ <input type="submit" value="登録" />
+</form>
+</body>
+</html>
+<?php
+/*
+＊氏名
+＊電話番号
+＊メールアドレス
+＊都道府県
+＊年齢
+＊備考欄
+＊利用規約に同意する
+
+
+*/
