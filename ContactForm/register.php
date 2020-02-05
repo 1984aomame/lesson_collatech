87156b9f92df89d1bd293ba232c672598134dd70
1984aomame <kafka.on.the.shoree@gmail.com>
2020-02-05 16:56:04 +0900

    【SQL】データベースへの送信文作成

--- ContactForm/register.php
+++ ContactForm/register.php
@@ -8,7 +8,53 @@
 </head>
 <body>
 <form action="confilm.php" method ="post">
-    <h3>登録完了です</h3>
+    <h3>登録結果表示</h3>
 </form>
 </body>
 </html>
+<?php
+if (isset($_POST["name"], $_POST["tel"], $_POST["mail"], $_POST["pref"], $_POST["age"], $_POST["message"])) {
+    $name = $_POST["name"];
+    $tel = $_POST["tel"];
+    $mail = $_POST["mail"];
+    $pref = $_POST["pref"];
+    $age = $_POST["age"];
+    $message = $_POST["message"];
+}
+// データベースに接続
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=resavation;charset=utf8',
+    'collatech',
+    'passeord',
+    array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET CHARACTER SET'utf8'")
+);
+// データベースに繋がっているかどうか確認
+if ($pdo) {
+    // DBに繋がっている
+} else {
+    echo"データベースに繋がっていません";
+}
+
+$regist = $pdo->prepare("INSERT INTO student(name, tel, mail, pref, age, message) VALUES(:name, :tel, :mail, :pref, :age, :message)");
+
+$regist -> bindParam (':name', $name);
+$regist -> bindParam (':tel', $tel);
+$regist -> bindParam (':mail', $mail);
+$regist -> bindParam (':pref', $pref);
+$regist -> bindParam (':age', $age);
+$regist -> bindParam (':message', $message);
+
+$regist -> execute(array($name, $tel, $mail, $pref, $age, $message));
+
+if ($regist) {
+    echo "登録完了です";
+} else (
+    echo "登録エラーです";
+)
+
+
+
+
+
+
+?>
