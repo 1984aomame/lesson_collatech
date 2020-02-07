fe0d3493aeb7fc489b5650b9ae2087bb38294e1b
1984aomame <kafka.on.the.shoree@gmail.com>
2020-02-07 10:26:12 +0900

    修正　＄POST送信をデーターベースへ作成中

--- ContactForm/confirm.php
+++ ContactForm/confirm.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,59 +7,46 @@
     <title>プログラミング講座申込み確認</title>
 </head>
 <body>
-<h2>プログラム体験申込確認</h2>
-<form method="POST" action ="http://localhost/lesson_collatech/ContactForm/register.php">
-    <table>
-    <tr>
-     <td>氏名</td> 
-     <td><?php echo $_POST["name"]; ?>  
-    <input id="name" type="hidden"  name="name" value=>
-     </td>
-    </tr>
-    <tr>
-     <td>電話番号</td> 
-     <td><?php echo $_POST["tel"]; ?>
-    
-     </td>  
-    </tr>
-    <tr>
-     <td>メールアドレス</td>
-     <td><?php echo $_POST["mail"]; ?> 
-     
-     </td>  
-    </tr>
-    <tr>
-     <td>都道府県</td>
-     <td><?php echo $_POST["pref"]; ?> 
-     
-     </td>   
-    </tr>
-    <tr>
-     <td>年齢</td>
-     <td><?php echo $_POST["age"]; ?>
-    
-     </td>    
-    </tr>
-    <tr>
-     <td>備考欄</td> 
-     <td><?php echo $_POST["message"]; ?>
-     </td>   
-    </tr>
-    </table>
- <input type="submit" value="登録" />
-</input>
-</form>
+    <h2>プログラム体験申込確認</h2>
+    <form method="POST"  action ="http://localhost/lesson_collatech/ContactForm/register.php">
+        <table>
+        <tr>
+            <td>氏名</td> 
+            <td><?php echo $_POST["name"]; ?> 
+            <input id="name" type="hidden" value="<?php $_POST["name"]; ?>">
+        </tr> 
+        <tr>
+            <td>電話番号</td> 
+            <td><?php echo $_POST["tel"]; ?>
+            <input id="tel" type="hidden" value="<?php $_POST["tel"]; ?> ">
+            </td>  
+        </tr>
+        <tr>
+            <td>メールアドレス</td>
+            <td><?php echo $_POST["mail"]; ?>
+            <input id="mail" type="hidden" value="<?php $_POST["mail"]; ?>">
+            </td>  
+        </tr>
+        <tr>
+            <td>都道府県</td>
+            <td><?php echo $_POST["pref"]; ?>
+            <input id="pref" type="hidden" value="<?php $_POST["pref"]; ?>">
+            </td>   
+        </tr>
+        <tr>
+            <td>年齢</td>
+            <td><?php echo $_POST["age"]; ?>
+            <input id="age" type="hidden" value="<?php $_POST["age"]; ?>">
+            </td>    
+        </tr>
+        <tr>
+            <td>備考欄</td> 
+            <td><?php echo $_POST["message"]; ?>
+            <input id="message" type="hidden" value="<?php $_POST["message"];?>">
+            </td>   
+        </tr>
+        </table>
+        <input type="submit" value="登録" /></input>
+    </form>
 </body>
 </html>
-<?php
-/*
-＊氏名
-＊電話番号
-＊メールアドレス
-＊都道府県
-＊年齢
-＊備考欄
-＊利用規約に同意する
-
-
-*/
